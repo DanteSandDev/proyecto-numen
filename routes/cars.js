5e44654b783c0da2c589e34960fb88e35a5c503a
Dante Sandoval <dantesandoval.dev@gmail.com>
2023-03-16 19:19:21 -0300

    Feature - cars routes validated

--- routes/cars.js
+++ routes/cars.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const carsController = require("../controllers/carsController")
+const carsController = require("../controllers/carsController");
+const { check } = require("express-validator");
 
 router.get("/", carsController.getCars);
 
@@ -8,10 +9,28 @@ router.get("/buscar", carsController.getCarByTipo)
 
 router.get("/:id", carsController.getCarById)
 
-router.post("/nuevoauto", carsController.postCar)
+router.post("/nuevoauto", [
+    check("marca").not().isEmpty().withMessage("La marca del auto es obligatoria"),
+    check("tipo").not().isEmpty().withMessage("El tipo del auto es obligatorio"),
+    check("modelo").not().isEmpty().withMessage("El modelo del auto es obligatorio").not().isNumeric().withMessage("El modelo debe ser numérico"),
+    check("color").not().isEmpty().withMessage("El color del auto es obligatorio"),
+    check("precio").not().isEmpty().withMessage("El precio del auto es obligatorio").not().isNumeric().withMessage("El precio debe ser numérico")
+], carsController.postCar)
 
-router.put("/actualizar/:id", carsController.putCar)
+router.put("/actualizar/:id", [
+    check("marca").not().isEmpty().withMessage("La marca del auto es obligatoria"),
+    check("tipo").not().isEmpty().withMessage("El tipo del auto es obligatorio"),
+    check("modelo").not().isEmpty().withMessage("El modelo del auto es obligatorio").not().isNumeric().withMessage("El modelo debe ser numérico"),
+    check("color").not().isEmpty().withMessage("El color del auto es obligatorio"),
+    check("precio").not().isEmpty().withMessage("El precio del auto es obligatorio").not().isNumeric().withMessage("El precio debe ser numérico")
+], carsController.putCar)
 
-router.delete("/eliminarAuto/:id", carsController.deleteCar)
+router.delete("/eliminarAuto/:id", [
+    check("marca").not().isEmpty().withMessage("La marca del auto es obligatoria"),
+    check("tipo").not().isEmpty().withMessage("El tipo del auto es obligatorio"),
+    check("modelo").not().isEmpty().withMessage("El modelo del auto es obligatorio").not().isNumeric().withMessage("El modelo debe ser numérico"),
+    check("color").not().isEmpty().withMessage("El color del auto es obligatorio"),
+    check("precio").not().isEmpty().withMessage("El precio del auto es obligatorio").not().isNumeric().withMessage("El precio debe ser numérico")
+], carsController.deleteCar)
 
 module.exports = router;
